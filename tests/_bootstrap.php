df549028357e4160a99ed652646509513b8b4b27
Andrii Vasyliev <sol@hiqdev.com>
2016-06-10 12:20:23 +0000

    csfixed

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Advanced Grid for Yii2
+ *
+ * @link      https://github.com/hiqdev/yii2-higrid
+ * @package   yii2-higrid
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015-2016, HiQDev (http://hiqdev.com/)
+ */
+
 error_reporting(E_ALL & ~E_NOTICE);
 
 $bootstrap = __DIR__ . '/../src/_bootstrap.php';
